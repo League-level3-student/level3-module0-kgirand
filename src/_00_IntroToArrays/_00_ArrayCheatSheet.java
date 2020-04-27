900d29a51e58b9d2d606483030800a5bee80e774
kgirand <44352919+kgirand@users.noreply.github.com>
2020-04-27 15:30:38 -0700

    level 3 mod 0 1st day
    arrays

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -5,22 +5,29 @@ import java.util.Random;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-
+		String strings[] = new String[4];
 		//2. print the third element in the array
-
+		System.out.println(strings[2]);
 		//3. set the third element to a different value
-
+		strings[2] = "hello";
 		//4. print the third element again
-		
+		System.out.println(strings[2]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
-		
+		for(int i = 0; i < strings.length; i++) {
+			strings[i] = "yes";
+		}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i = 0; i < strings.length; i++) {
+			System.out.println(strings[i]);
+		}
 		//7. make an array of 50 integers
-
+		int ints[] = new int[50];
 		//8. use a for loop to make every value of the integer array a random number
-
+		Random randy = new Random();
+		for(int i = 0; i < ints.length; i++) {
+			ints[i] = randy.nextInt();
+		}
 		//9. without printing the entire array, print only the smallest number on the array
 
 		//10 print the entire array to see if step 8 was correct
